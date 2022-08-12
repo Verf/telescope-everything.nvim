a7c50f1ec75e0d287401329eaf996833a6e8b0ce
Verf <verf@protonmail.com>
2022-08-12 18:21:42 +0800

    finish all basic feature

--- lua/telescope/_extensions/everything.lua
+++ lua/telescope/_extensions/everything.lua
@@ -1,59 +1,65 @@
+local has_telescope, _ = pcall(require, "telescope")
+if not has_telescope then
+	error("This plugin requires nvim-telescope/telescope.nvim")
+end
+
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local sorters = require("telescope.sorters")
 local conf = require("telescope.config").values
 local make_entry = require("telescope.make_entry")
 
-local M = {}
+local flatten = vim.tbl_flatten
 
-local es_opts = {}
+local es_config = {
+	max_results = 100,
+}
 
-function M.setup(opts)
-	es_opts.case_sensitive = opts.case_sensitive or false
-	es_opts.regex = opts.regex or false
-	es_opts.whole_word = opts.whole_word or false
-	es_opts.match_path = opts.match_path or false
-	es_opts.offset = opts.offset or 0
-	es_opts.max_results = opts.max_results or 100
-	es_opts.sort = opts.sort or false
+local function es_setup(ext_config)
+	for k, v in pairs(ext_config) do
+		es_config[k] = v
+	end
 end
 
-function M.everything(opts)
+local run = function(opts)
+	opts = vim.tbl_deep_extend("force", es_config, opts or {})
 	local find_command = { "es" }
-	if es_opts.case_sensitive then
+	if opts.case_sensitive then
 		table.insert(find_command, "-case")
 	end
-	if es_opts.regex then
-		table.insert(find_command, "-regex")
-	end
-	if es_opts.whole_word then
+	if opts.whole_word then
 		table.insert(find_command, "-whole-word")
 	end
-	if es_opts.match_path then
+	if opts.match_path then
 		table.insert(find_command, "-match-path")
 	end
-	if es_opts.sort then
+	if opts.sort then
 		table.insert(find_command, "-s")
 	end
-	if es_opts.offset then
+	if opts.offset then
 		table.insert(find_command, "-offset")
-		table.insert(find_command, tostring(es_opts.offset))
+		table.insert(find_command, tostring(opts.offset))
 	end
-	if es_opts.max_results then
+	if opts.max_results then
 		table.insert(find_command, "-max-results")
-		table.insert(find_command, tostring(es_opts.max_results))
+		table.insert(find_command, tostring(opts.max_results))
 	end
+	_G.print_tb(find_command)
 	pickers.new(opts, {
 		prompt_title = "Everything",
-		finder = finders.new_job(find_command, opts.entry_maker or make_entry.gen_from_string),
-		previewer = conf.grep_previewer(opts),
-		sorter = sorters.highlighter_only(opts),
+		finder = finders.new_job(function(prompt)
+			if prompt then
+				return flatten({ find_command, prompt })
+			end
+			return find_command
+		end, opts.entry_maker or make_entry.gen_from_file(opts)),
+		previewer = conf.file_previewer(opts),
+		sorter = conf.file_sorter(opts),
 	}):find()
 end
 
 return require("telescope").register_extension({
-	setup = M.setup,
+	setup = es_setup,
 	exports = {
-		everything = M.everything,
+		everything = run,
 	},
 })
